24b72e292223bf2da69627870449f4ef307e35b3
boysbee <gogorenger@gmail.com>
2015-08-15 01:47:28 +0700

    Add ItemController.

---
+++ src/main/java/com/demo/springboot/ItemController.java
@@ -0,0 +1,38 @@
+package com.demo.springboot;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * Created by boysbee on 8/15/2015 AD.
+ */
+@RestController
+@RequestMapping("/items")
+public class ItemController {
+    @Autowired
+    private ItemRepository repo;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public List findItems() {
+        return repo.findAll();
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public Item addItem(@RequestBody Item item) {
+        item.setId(null);
+        return repo.saveAndFlush(item);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    public Item updateItem(@RequestBody Item updatedItem, @PathVariable Integer id) {
+        updatedItem.setId(id);
+        return repo.saveAndFlush(updatedItem);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public void deleteItem(@PathVariable Integer id) {
+        repo.delete(id);
+    }
+}
