f96b7a6702a4024ae6ee0f17e9d11f5d45d05d66
nattaporn.c <gogorenger@gmail.com>
2015-08-15 12:09:59 +0700

    Add unit test.

--- src/test/java/com/demo/springboot/SpringBootAngularApplicationTests.java
+++ src/test/java/com/demo/springboot/SpringBootAngularApplicationTests.java
@@ -1,16 +1,42 @@
 package com.demo.springboot;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+
+@WebAppConfiguration
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = SpringBootAngularApplication.class)
 public class SpringBootAngularApplicationTests {
+    final String BASE_URL = "http://localhost:8080/";
+    private MockMvc mockMvc;
+    @Autowired
+    private WebApplicationContext webappContext;
 
-	@Test
-	public void contextLoads() {
-	}
+    @Before
+    public void setup() {
+        this.mockMvc = webAppContextSetup(this.webappContext).build();
+    }
+
+    @Test
+    public void test_items() throws Exception {
+        this.mockMvc.perform(get("/items").accept(MediaType.parseMediaType("application/json;charset=UTF-8")))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("application/json;charset=UTF-8"));
+
+    }
+
 
 }
